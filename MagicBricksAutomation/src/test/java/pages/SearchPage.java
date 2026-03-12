ce718e79abce67bd23ea9dcfe37cc4538d3b45c0
Madhav005 <madhavmathi142k5@gmail.com>
2026-03-12 13:24:54 +0530

    scroll update

--- MagicBricksAutomation/src/test/java/pages/SearchPage.java
+++ MagicBricksAutomation/src/test/java/pages/SearchPage.java
@@ -1,29 +1,93 @@
 package pages;
 
-import org.openqa.selenium.*;
-import org.openqa.selenium.support.*;
 import java.util.List;
 
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class SearchPage {
+    WebDriver driver;
 
-WebDriver driver;
+    
+    @FindBy(className = "mb-srp__list")
+    private List<WebElement> propertyCards;
 
-public SearchPage(WebDriver driver){
+    
+    @FindBy(css = ".mb-srp__card__photo__fig--heart") 
+    private List<WebElement> shortlistIcons;
 
-this.driver = driver;
-PageFactory.initElements(driver,this);
+    public SearchPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
 
-}
+    
+     
+    public void shortlistHouses(int count) {
+        for (int i = 0; i < count && i < shortlistIcons.size(); i++) {
+            try {
+                WebElement heart = shortlistIcons.get(i);
+                if (heart.isDisplayed()) {
+                    heart.click();
+                    System.out.println("Shortlisted house #" + (i + 1));
+                    // Brief sleep might be needed if the site has heavy JS overlays
+                    Thread.sleep(500); 
+                }
+            } catch (Exception e) {
+                System.out.println("Could not click heart icon at index " + i + ": " + e.getMessage());
+            }
+        }
+    }
+    
+    public void scrollUntilPropertiesLoad()
+    {
 
-@FindBy(xpath="//div[contains(@class,'mb-srp__card')]")
-List<WebElement> propertyCards;
+    JavascriptExecutor js = (JavascriptExecutor) driver;
 
-@FindBy(xpath="//button[contains(text(),'Compare')]")
-WebElement compareButton;
+    long lastHeight = (long) js.executeScript("return document.body.scrollHeight");
 
-public void shortlistProperties(){
+    while(true)
+    {
 
+    js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
 
+    try {
+    Thread.sleep(2000);   // wait for new properties to load
+    } catch (InterruptedException e) {
+    e.printStackTrace();
+    }
 
-}
+    long newHeight = (long) js.executeScript("return document.body.scrollHeight");
+
+    if(newHeight == lastHeight)
+    {
+    break;
+    }
+
+    lastHeight = newHeight;
+
+    }
+
+    }
+    
+    public void scrollPage()
+    {
+
+    JavascriptExecutor js = (JavascriptExecutor) driver;
+
+    for(int i=0; i<5; i++)
+    {
+
+    js.executeScript("window.scrollBy(0,2000)");
+
+    try {
+    Thread.sleep(2000);
+    } catch(Exception e){}
+
+    }
+
+    }
 }
--- MagicBricksAutomation/src/test/java/tests/MagicBricksTest.java
+++ MagicBricksAutomation/src/test/java/tests/MagicBricksTest.java
@@ -4,6 +4,7 @@ import org.testng.annotations.Test;
 
 import base.BaseTest;
 import pages.HomePage;
+import pages.SearchPage;
 
 public class MagicBricksTest extends BaseTest {
 
@@ -13,6 +14,9 @@ public void propertyShortlistTest(){
 	HomePage home = new HomePage(driver);
 
 	home.searchCity("Bangalore");
+	SearchPage searchPage = new SearchPage(driver);
+	//searchPage.scrollUntilPropertiesLoad();
+	searchPage.scrollPage();
     
 	
 
