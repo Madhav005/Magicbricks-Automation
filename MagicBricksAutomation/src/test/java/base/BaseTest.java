b406b3c7a2a649d0cd2212266e122bf0e05f93b4
dhivyapriyar <dhivyapriya2611@gmail.com>
2026-03-12 12:16:27 +0530

    base, home page updated

--- MagicBricksAutomation/src/test/java/base/BaseTest.java
+++ MagicBricksAutomation/src/test/java/base/BaseTest.java
@@ -1,5 +1,27 @@
 package base;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
 public class BaseTest {
+
+public static WebDriver driver;
+
+@BeforeMethod
+public void setup(){
+
+driver = new ChromeDriver();
+driver.manage().window().maximize();
+driver.get("https://www.magicbricks.com");
+
+}
+
+@AfterMethod
+public void tearDown(){
+
+driver.quit();
+
+}
 
 }
--- MagicBricksAutomation/src/test/java/pages/HomePage.java
+++ MagicBricksAutomation/src/test/java/pages/HomePage.java
@@ -1,5 +1,29 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.*;
+
 public class HomePage {
+
+WebDriver driver;
+
+public HomePage(WebDriver driver){
+this.driver = driver;
+PageFactory.initElements(driver,this);
+}
+
+@FindBy(id="keyword")
+WebElement cityInput;
+
+@FindBy(xpath="//button[contains(text(),'Search')]")
+WebElement searchButton;
+
+public void searchCity(String city){
+
+cityInput.sendKeys(city);
+searchButton.click();
+
+}
 
 }
